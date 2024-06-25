cc8a2c181c16e2372da4d2f2833776b9b0e09277
Isaac Ito <72858790+Isaac-Ito-MW@users.noreply.github.com>
2024-06-25 16:03:40 +0100

    Update atProjectStartUp.m

--- Utility/atProjectStartUp.m
+++ Utility/atProjectStartUp.m
@@ -4,10 +4,10 @@ function atProjectStartUp
 % To change the automatic execution setting, select the Project window,
 % and in the PROJECT toolstrip, click Startup Shutdown.
 %
-% Copyright 2020-2023 The MathWorks, Inc.
+% Copyright 2020-2024 The MathWorks, Inc.
 
-if not(contains(matlabRelease().Release, "R2023b"))
-  disp("This project was developed in R2023b.")
+if not(contains(matlabRelease().Release, "R2024a"))
+  disp("This project was developed in R2024a.")
   relstr = matlabRelease().Release;
   disp("This MATLAB Release is " + relstr(2:end-1) + ".")
 end
