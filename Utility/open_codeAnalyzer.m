00ac276c019eb8aa4d198420c856f2485ffaf315
Isaac Ito <130740375+isaacito12@users.noreply.github.com>
2024-06-25 15:53:51 +0100

    Upgrade to R2024a. Add Vehicle1D performance design app. (#21)
    * Update HTML files to open models or scripts from hyperlinks. Save all model files in R2023a.
    * Use buildtools in Battery High Voltage component
    * Update buildfile.m at the project root.
    * Add buildtool-results folder to the project.
    * Generate Markdowns instead of Jupyter notebooks.
    * Add Vehicle1D performance design app.
    * Update .github/workflows/ci.yml file to use R2024a in Actions.

---
+++ Utility/open_codeAnalyzer.m
@@ -0,0 +1,8 @@
+% This script opens the Code Analyzer.
+%
+% Documentation
+%
+% - Code Analyzer
+%   https://www.mathworks.com/help/matlab/ref/codeanalyzer-app.html
+
+codeAnalyzer(currentProject().RootFolder)
--- buildfile.m
+++ buildfile.m
@@ -5,7 +5,7 @@ function plan = buildfile
 
 top_folder = currentProject().RootFolder;
 
-test_definitions = [
+test_definitions = [ 
   fullfile(top_folder, "BEV", "Test", "BEV_UnitTest_MQC.m")
   ...
   fullfile(top_folder, "Components", "BatteryHighVoltage", "Test", "BatteryHV_UnitTest.m")
@@ -17,7 +17,9 @@ test_definitions = [
   ...
   fullfile(top_folder, "Components", "MotorDriveUnit", "Test", "MotorDriveUnit_UnitTest_MQC.m")
   ...
+  fullfile(top_folder, "Components", "Vehicle1D", "Test", "Vehicle1D_UnitTest.m")
   fullfile(top_folder, "Components", "Vehicle1D", "Test", "Vehicle1D_UnitTest_MQC.m")
+  fullfile(top_folder, "Components", "Vehicle1D", "Test", "Vehicle1DPerformanceDesignApp_uitest.m")
   ...
   fullfile(top_folder, "Components", "VehicleSpeedReference", "Test", "VehSpdRef_UnitTest_MQC.m")
   ...
@@ -80,7 +82,7 @@ plan("Test") = matlab.buildtool.tasks.TestTask( ...
   "cache/buildtool-results/code-coverage.xml"
   ] );
 
-plan("Test").Dependencies = ["CodeIssues" "CheckProject"];
+plan("Test").Dependencies = ["CodeIssues"];
 
 plan("Test").Tests = test_definitions;
 
