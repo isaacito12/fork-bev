f3b953c7f89649c01048312ce41dc0ddfb645817
Isaac Ito <130740375+isaacito12@users.noreply.github.com>
2024-11-20 16:13:26 +0000

    Update Vehicle1D component to test exported Markdown files are up to date.

--- Utility/LocalTasks/generateMarkdown_fromLiveScript.m
+++ Utility/LocalTasks/generateMarkdown_fromLiveScript.m
@@ -1,4 +1,4 @@
-function generateMarkdown_fromLiveScript(NameValuePairs)
+function NumConversions = generateMarkdown_fromLiveScript(NameValuePairs)
 %% Generates Markdown files from Live Scripts
 
 % This function finds project-registered Live Scripts in the specified folder
@@ -11,6 +11,10 @@ function generateMarkdown_fromLiveScript(NameValuePairs)
 % If an up-to-date Markdown file already exists for the corresponding Live Script,
 % no new Markdown file is generated. To generate a Markdown file,
 % set ForceExport option to true.
+%
+% By default, this function displays messages about Live script files found and
+% where exported Markdown files were saved.
+% To surpress messages, set Quiet option to true.
 
 % Copyright 2024 The MathWorks, Inc.
 
@@ -18,10 +22,15 @@ arguments (Input)
   NameValuePairs.LiveScriptFolderNames (:,1) string {mustBeFolder} = "."
   NameValuePairs.MarkdownFolderPath (1,1) string = "markdown"
   NameValuePairs.ForceExport (1,1) logical = false
+  NameValuePairs.DisplayInfo (1,1) logical = false
 end
+
+display_info = NameValuePairs.DisplayInfo;
 
 project_files = currentProject().Files';
 project_file_paths = [project_files.Path]';
+
+NumConversions = 0;
 
 num_folders = numel(NameValuePairs.LiveScriptFolderNames);
 
@@ -43,29 +52,37 @@ for k = 1 : num_folders
 
   num_files = numel(mlxfiles_fullpaths);
   if num_files == 0
-    disp("No project-registered Live Script was found in " + live_script_folder_path)
+    if display_info
+      disp("No project-registered Live Script was found in " + live_script_folder_path)
+    end  % if
     continue
-  end
+  end  % if
 
-  disp(">>> Exporting live scripts in the folder: " + live_script_folder_path)
-
-  if num_files == 1
-    disp(">>> 1 Live Script found.")
-  elseif num_files > 1
-    disp(">>> " + num_files + " Live Scripts found.")
-  end
+  if display_info
+    disp(">>> Exporting live scripts in the folder: " + live_script_folder_path)
+    if num_files == 1
+      disp(">>> 1 Live Script found.")
+    elseif num_files > 1
+      disp(">>> " + num_files + " Live Scripts found.")
+    end  % if
+  end  % if
 
   exported = LiveScriptUtility2.CheckAndGenerateMarkdown( ...
     mlxfiles_fullpaths, ...
     MarkdownFolderPath = NameValuePairs.MarkdownFolderPath, ...
-    ForceExport = NameValuePairs.ForceExport );
+    ForceExport = NameValuePairs.ForceExport, ...
+    DisplayInfo = display_info );
 
   num_exported = nnz(exported);
-  if num_exported == 1
-    disp("1 Live Script was exported.")
-  elseif num_exported > 1
-    disp(num_exported + " Live Scripts were exported to Markdown files.")
-  end
+  NumConversions = NumConversions + num_exported;
+
+  if display_info
+    if num_exported == 1
+      disp("1 Live Script was exported.")
+    elseif num_exported > 1
+      disp(num_exported + " Live Scripts were exported to Markdown files.")
+    end  % if
+  end  % if
 
 end  % for
 
